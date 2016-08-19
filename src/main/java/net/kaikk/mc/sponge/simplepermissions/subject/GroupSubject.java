6f2e73528fcf30568eb1aa9ba4077d6e58e144f4
Kai <kai.kikuchi27@gmail.com>
2016-08-19 13:00:39 +0200

    Bugfix group permissions inheritance not working properly.

--- src/main/java/net/kaikk/mc/sponge/simplepermissions/subject/GroupSubject.java
+++ src/main/java/net/kaikk/mc/sponge/simplepermissions/subject/GroupSubject.java
@@ -22,15 +22,15 @@ import net.kaikk.mc.sponge.simplepermissions.SimplePermissions;
 public class GroupSubject extends SimpleSubject {
 	private GroupSubject parent;
 	private int weight;
-	
+
 	public GroupSubject(String identifier, GroupSubjectCollection collection) {
 		this(identifier, collection, 0);
 	}
-	
+
 	public GroupSubject(String identifier, GroupSubjectCollection collection, int weight) {
 		this(identifier, collection, weight, null);
 	}
-	
+
 	public GroupSubject(String identifier, GroupSubjectCollection collection, int weight, GroupSubject parent) {
 		super(identifier, collection);
 		this.weight = weight;
@@ -40,7 +40,7 @@ public class GroupSubject extends SimpleSubject {
 	public GroupSubject getParent() {
 		return parent;
 	}
-	
+
 	public int getWeight() {
 		return weight;
 	}
@@ -49,12 +49,12 @@ public class GroupSubject extends SimpleSubject {
 	public boolean isChildOf(Set<Context> contexts, Subject parent) {
 		return this.parent.equals(parent);
 	}
-	
+
 	@Override
 	public List<Subject> getParents(Set<Context> contexts) {
 		return Lists.newArrayList(this.parent);
 	}
-	
+
 	@Override
 	public SubjectCollection getContainingCollection() {
 		return SimplePermissions.instance().getGroupSubjects();
@@ -67,78 +67,81 @@ public class GroupSubject extends SimpleSubject {
 	public void setWeight(int weight) {
 		this.weight = weight;
 	}
-	
+
 	@Override
 	public Tristate getPermissionValue(Set<Context> contexts, String permission) {
 		Tristate tristate = super.getPermissionValue(contexts, permission);
 		if (tristate!=Tristate.UNDEFINED) {
 			return tristate;
 		}
-		
+
 		Optional<GroupSubject> group = this.getHeaviestParentGroupFor(permission);
 		if (group.isPresent()) {
 			return group.get().getPermissionValue(null, permission);
 		}
-		
+
 		return this.getDefaultPermissionValue(permission);
 	}
-	
+
 	public Tristate getDefaultPermissionValue(String permission) {
 		if (this.equals(((SimpleSubjectCollection) this.getContainingCollection()).getDefaults())) {
 			return Tristate.UNDEFINED;
 		}
-		
+
 		return ((SimpleSubjectCollection) this.getContainingCollection()).getDefaults().getPermissionValue(null, permission);
 	}
-	
+
 	public Optional<GroupSubject> getHeaviestParentGroupFor(String permission) {
 		if (this.parent==null) {
 			return Optional.empty();
 		}
-		
+
 		permission = permission.toLowerCase();
+
+		Boolean b = parent.getSubjectData().getPermissions(null).get(permission);
+
 		Optional<GroupSubject> g = parent.getHeaviestParentGroupFor(permission);
-		if (g.isPresent() && g.get().getWeight()>parent.getWeight()) {
-			Boolean b = g.get().getSubjectData().getPermissions(null).get(permission);
-			if (b!=null) {
+		if (g.isPresent() && (g.get().getWeight()>parent.getWeight() || b==null)) {
+			Boolean b2 = g.get().getSubjectData().getPermissions(null).get(permission);
+			if (b2!=null) {
 				return g;
 			}
 		}
-		Boolean b = parent.getSubjectData().getPermissions(null).get(permission);
+
 		if (b!=null) {
 			return Optional.of(parent);
 		}
 		return Optional.empty();
 	}
-	
+
 	@Override
 	public Text info() {
 		Builder b = Text.builder();
 		b.append(Text.of(TextColors.GREEN, "-- SimplePermissions - Group ", TextColors.GOLD, this.getIdentifier(), TextColors.GREEN, " --", Text.NEW_LINE));
-		
+
 		if (this.parent!=null) {
 			b.append(Text.of(TextColors.GREEN, "Parent: ", TextColors.AQUA, this.parent.getIdentifier(), Text.NEW_LINE));
 		}
-		
+
 		if (this.weight!=0) {
 			b.append(Text.of(TextColors.GREEN, "Weight: ", TextColors.AQUA, this.weight, Text.NEW_LINE));
 		}
-		
+
 		b.append(Text.of(TextColors.GREEN, "Permissions:", Text.NEW_LINE));
 		for(Entry<String,Boolean> e : this.getSubjectData().getPermissions(null).entrySet()) {
 			b.append(e.getValue() ? Text.of(TextColors.GREEN, "+ ") : Text.of(TextColors.RED, "- "), Text.of(TextColors.AQUA, e.getKey()), Text.NEW_LINE);
 		}
-		
+
 		return b.build();
 	}
-	
+
 	@Override
 	public Map<Set<Context>, List<Subject>> getAllParents() {
 		Map<Set<Context>, List<Subject>> map = new HashMap<Set<Context>, List<Subject>>();
 		map.put(null, this.getParents());
 		return map;
 	}
-	
+
 	public boolean parentCheck(GroupSubject other) {
 		if (other==this) {
 			return true;
