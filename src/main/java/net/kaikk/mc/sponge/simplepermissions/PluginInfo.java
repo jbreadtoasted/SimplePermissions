70f15d7c6975f1737f30c5e90cc899a938204e64
Kai <kai.kikuchi27@gmail.com>
2016-10-09 06:40:13 +0200

    Version 0.9.14

--- src/main/java/net/kaikk/mc/sponge/simplepermissions/PluginInfo.java
+++ src/main/java/net/kaikk/mc/sponge/simplepermissions/PluginInfo.java
@@ -3,7 +3,7 @@ package net.kaikk.mc.sponge.simplepermissions;
 public class PluginInfo {
 	public static final String id = "simplepermissions";
 	public static final String name = "SimplePermissions";
-	public static final String version = "0.9.13";
+	public static final String version = "0.9.14";
 	public static final String description = "A very simple basic permissions plugin";
 	
 	private PluginInfo(){}
